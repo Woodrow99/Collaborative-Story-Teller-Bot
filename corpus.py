9f5977fb9e3662b78ae1a7b4be0571164bc79aed
drake-oswald-cs <drake.oswald.cs@gmail.com>
2020-04-06 11:04:44 -0500

    Add files via upload

--- corpus.py
+++ corpus.py
@@ -17,40 +17,27 @@ def create_corpus():
     wordLists = PlaintextCorpusReader(os.getcwd(), '.*')
     textList = wordLists.fileids()
 
+    normSentsList = []
+
     for i in textList:
         
-        normSentsList = []
-        SentList = []
-        sentsList = wordLists.sents(i)
-        for j in range(0, len(sentsList)):
-            temp = sentsList[j]
-            SentList.append(temp)
-            temp = [word.lower() for word in temp]
-            temp = [re.sub('\W+','', word) for word in temp]
-            #temp = [word for word in temp if not word in stop_words]
-            temp = [word for word in temp if not word in blanks]
-            temp = [ps.stem(word) for word in temp]
-            normSentsList.append(temp)
-        os.chdir(og_dir)
-        os.chdir(og_dir + "/corpus_text")
-        fle = open(i[:-4] + "_reduce.json", "w+") 
-        fle.write(json.dumps(normSentsList))
-        fle = open(i[:-4] + "_full.json", "w+") 
-        fle.write(json.dumps(SentList))
-        os.chdir(og_dir)
-        os.chdir(og_dir + "/raw")
+        opened = open(i,'r')
+        readable = opened.read()
+        string_list = readable.split()
 
-    
-"""
-og_dir = os.getcwd()
-os.chdir(og_dir)
-os.chdir(og_dir + "/corpus_text")
-lst = os.listdir()
-print(lst)
-for doc in lst:
-    with open(doc) as json_file:
-        data = json.load(json_file)
-        print(len(data))
-"""
+        for word in string_list:
+            word = word.lower()
+            word = re.sub('\W+','', word)
+            normSentsList.append(word)
 
+    start = 0
+    end = 50
+    sequenceList = []
+    while end <= len(normSentsList):
+        sequenceList.append(normSentsList[start:end])
+        start += 1 
+        end += 1
+    fle = open("training_sequences", "w+")
+    fle. write(json.dumps(sequenceList))
+            
 create_corpus()
