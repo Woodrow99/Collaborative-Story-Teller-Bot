6a4d383b5ef2966c8907ebe6c8717ab665cebf65
NoodleCup <61511066+NoodleCup@users.noreply.github.com>
2020-04-09 16:25:45 -0500

    finalized version of rnn

--- RNN.py
+++ RNN.py
@@ -1,77 +1,43 @@
-import json
+from pickle import dump
 
-from keras_preprocessing.text import Tokenizer
-from keras.engine.saving import load_model
 from keras.models import Sequential
-from keras.layers import Dense, Embedding, Flatten
-from keras.layers import LSTM
-from keras.optimizers import Adam, RMSprop
-import numpy as np
-from keras.utils import np_utils
-import sys
+from keras.layers import Dense, Embedding, Bidirectional, LSTM
+from keras.optimizers import Adam
+from numpy import array
+from keras.utils.np_utils import to_categorical
+import os
+from keras_preprocessing.text import Tokenizer
+from nltk.corpus import PlaintextCorpusReader
 
-with open('corpus_text/anne-of-green-gables_full.json') as json_file:
-    text = json.load(json_file)
-    words = []
-    for sent in text:
-        for word in sent:
-            words.append(word)
-    setWords = sorted(list(set(words)))
-    print('total words:', len(words))
-    wordIndices = {}
-    indicesWord = {}
-    num = 0
-    for word in setWords:
-        wordIndices[word] = num
-        indicesWord[num] = word
-        num += 1
 
-sequence_max = 50
-dataX = []
-dataY = []
-for i in range(0, len(words) - sequence_max, sequence_max + 1):  # generates sequence-nextWord pairs
-    sequence = words[i:i + sequence_max]
-    nextWord = words[i + sequence_max]
-    dataX.append([wordIndices[word] for word in sequence])
-    dataY.append(wordIndices[nextWord])
+def LoadDoc(filename):
+    file = open(filename, 'r')
+    text = file.read()
+    file.close()
+    return text
 
-X = np.array(dataX)
-y = np.array(dataY)
-y = np_utils.to_categorical(dataY, num_classes=len(setWords))
-print(X)
-print(y)
-
+og_dir = os.getcwd()
+os.chdir(og_dir + "/corpus")
+vocabText = LoadDoc("vocab.txt")
+vocabList = vocabText.split('\n')
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(vocabList)
+seqLength = 50
+vocabSize = len(tokenizer.word_index) + 1
+os.chdir(og_dir)
 
 def CreateModel():
     model = Sequential()
-    model.add(Embedding(len(setWords), 50))
-    model.add(LSTM(256, return_sequences=True))
-    model.add(LSTM(256))
-    model.add(Dense(256, activation='relu'))
-    model.add(Dense(y.shape[1], activation='softmax'))
+    model.add(Embedding(vocabSize, seqLength))
+    model.add(Bidirectional(LSTM(128, return_sequences=True)))
+    model.add(Bidirectional(LSTM(128)))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(vocabSize, activation='softmax'))
     print(model.summary())
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.fit(X, y, batch_size=128, epochs=100)
-    model.save('annemodel.h5')
+    optimizer = Adam()
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    model.save("model.h5")
+CreateModel()
+dump(tokenizer, open('tokenizer.pkl', 'wb'))
 
 
-def GenerateText(model, pattern):
-    start = np.random.randint(0, len(dataX) - 1)
-    pattern = dataX[start]
-    print("Seed:")
-    print("\"", ' '.join([indicesWord[value] for value in pattern]), "\"")
-    for i in range(1000):
-        x = np.array(pattern)
-        prediction = model.predict(x, verbose=0)
-        index = np.argmax(prediction)
-        print(index)
-        result = indicesWord[index]
-        seq_in = [indicesWord[value] for value in pattern]
-        sys.stdout.write(result + ' ')
-        pattern.append(index)
-        pattern = pattern[1:len(pattern)]
-    print("\nDone.")
-
-
-# CreateModel()
-GenerateText(load_model("annemodel.h5"), dataX[np.random.randint(0, len(dataX) - 1)])
